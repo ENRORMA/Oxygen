add0baf3396576abc9dcd3ee499da1d86c53f4e1
enrorma <enrorma@gmail.com>
2023-09-14 22:27:26 +0200

    hurensohn

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 #!/bin/python3
 
 #Python Version:	3.10
-#Author:			ENRORMA
-#Created:			14/9/2023
-#Modified:			14/9/2023
-#License:			GPL3
+#Author:		ENRORMA
+#Created:		14/9/2023
+#Modified:		14/9/2023
+#License:		GPL3
 #Oxygen Version:	0.1
 
 import os
