5d2ead6d94f3a3d03032ccbb3cfb6b0652832bd7
enrorma <enrorma@gmail.com>
2023-09-14 23:29:04 +0200

    hurensohn

--- Modules/ParsePass1.py
+++ Modules/ParsePass1.py
@@ -1,5 +1,14 @@
 def ParsePass1(LocalSourceFileContent):
     LocalSourceLines = LocalSourceFileContent.split("\n")
-    Line = LocalSourceLines[:1]
-    Line = "".join(Line)
-    print(Line)
+    LineCount = len(LocalSourceLines)
+    LineCounter = 0
+    while LineCounter < LineCount:
+        LineCounter += 1
+        Line = LocalSourceLines[:1]
+        print(Line)
+        Line = "".join(Line)
+        CommentCheck = Line.lstrip()[:1]
+        if CommentCheck != "#":
+            GoodLines = LocalSourceLines[:1]
+        LocalSourceLines = LocalSourceLines[1:]
+    #print(GoodLines)
